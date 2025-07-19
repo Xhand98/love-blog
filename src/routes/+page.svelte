d0206aa2d426ce140f77d207f6da1e7daa395995
xhand98 <hendrickherrera9@gmail.com>
2025-07-19 09:43:28 -0400

    repo init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
+	import Input from '$lib/components/ui/input/input.svelte';
+	const corrPass = ['vaionetta', 'titita', 'bonini', 'onyelkis'];
+
+	let input = $state('');
+</script>
+
+<main class="flex flex-col gap-y-5">
+	<h1 class="text-3xl font-bold text-violet-500">Hiiiiiiiiiiiiiiiiiiiiiiiii</h1>
+
+	<p class="text-2xl text-violet-500">
+		Esta pagina esta dedicada para mi mochito, si eres ella ingresa su apodo aqui abajo:
+	</p>
+	<Input type="text" id="pass" bind:value={input} class="w-52" />
+
+	{#if corrPass.includes(input.toLowerCase())}
+		<p aria-label="Link to next page" class="text-2xl text-violet-500">
+			MI amorcito bello, ERES TUUUUUUUUUUUUUUUUUUUUUUUU, ve a la siguiente pagina dandole click al
+			boton ->
+		</p>
+		<a href="/valid" class="hover:cursor-pointer">
+			<Button aria-label="Button to next page" class="bg-violet-400">Siguiente pagina</Button>
+		</a>
+	{/if}
+</main>
---
+++ src/routes/valid/+page.svelte
@@ -0,0 +1,22 @@
+<main class="flex min-h-screen items-center justify-center p-4">
+	<p class="w-96 text-center text-xl">
+		HIiiiiiiiiii, si adivinaste la contraseña... Bueno mi amor, permiteme ser sincero es que te amo
+		un mundo, graciaas por ser fuerte y no rendirte gracias por estar ahí cuando te necesito y
+		gracias por no irte de mi lado a pesar de actitudes que aveces tomo, esta mañana la tomare como
+		un mañana de agradecimiento ya que, aunque todo parece ir mal ahora, mi princesita bella no se
+		rinde aun y sigue luchando dia a dia para lograr sus metas, se que hay veces que solo te quieres
+		rendir, acostarte y no despertar, pero, te prometo que si seguimos luchndo y esforzandonos
+		llegaremos muy lejos, osea, muy lejos, como a 5e2000 millas de donde estamos ahora, pues confio
+		en nosotros y se que nuestro amor, nunca acabara. Mi vida hermosa, necesito que seas una
+		princesa fuerte y aguantes todo por ahora, se que es dificil pero así como hay momentos
+		estresantes, hay momentos buenos igual, hay oscuridad y hay luz, hay momentos de hambre y hay
+		momnetos donde ni quieres comer mas de tanto que comiste, solo quiero que sepas que te amo mucho
+		y siempre lo hare aunque por como actue aveces no lo demuestre... WAOOOOOOOOOOOO que dibujo mas
+		bello del mar mi vida, hay el mar, tan bello como tu, y tan profundo como mis sentimientos hacia
+		ti, aunque ahora no lo parezca y te sientas mal y parezca que no me gusto, si me gusto el
+		dibujito mi vida, esta bello. A est pagina ni estilo le voy a poner bueno si, poquito no quiero
+		entregarte algo feo. Creo que eso es todo por ahora, esta pagina la hare un blog o algo asi para
+		subir cositas para que las leas, bueno vida, voy a ponerle estilo a la pagina para que quede
+		bien
+	</p>
+</main>
